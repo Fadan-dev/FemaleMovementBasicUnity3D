c929c367bc9c3dc382a60187298daa82d4fc0283
Fadan-dev <fadel.ramadhan81299@gmail.com>
2021-06-29 13:56:50 +0700

    Init

---
+++ Scripts/CameraFemaleController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e01649e58cb09adf63f4d5ed8e6d7f908aa1a717ff4f4acd01f34bc342b0d8f2
+size 633
---
+++ Scripts/PlayerFemaleMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:75642ae5090c7f73e40894decd793e257df713b4c601242544ecdbf130978eb8
+size 2620
